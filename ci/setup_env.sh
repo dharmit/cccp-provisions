4f546d5be2435d32f18e074cf0c54336dfcafdd5
Ratnadeep Debnath <rtnpro@gmail.com>
2016-08-09 14:05:55 +0530

    Moved and updated CI scripts.

--- ci/setup_env.sh
+++
@@ -1,9 +0,0 @@
-if [ -f env.properties ]; then
-    rm env.properties
-fi
-touch env.properties
-echo "URL_BASE=http://admin.ci.centos.org:8080" >> env.properties
-echo "API=$(cat ~/duffy.key)" >> env.properties
-
-bash utils/gencert.sh registry.centos.org || true
-echo -e  'y\n'| ssh-keygen -t rsa -N "" -f jenkins.key
